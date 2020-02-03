ebaa82fce2d3b976929176033e07cb3838ef6125
Luis Belmontes <luisbelmontes@ku.edu>
2020-02-03 15:40:35 -0600

    Fixed search()

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -38,9 +38,17 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+	while(!isFound && temp != nullptr)
+	{
+		if(temp.getValue() == value)
+		{
+			isFound == true;
+		}
+		else
+		{
+			temp = temp.getNext()
+		}
+	}
 
 	return(isFound);
 }
